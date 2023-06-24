e530e54ce32d51c818046ecf4f41d41dce906e32
Dopzhik <haltura24@gmail.com>
2023-06-24 19:42:07 +0800

    Сделано домашнее задание

---
+++ TASK3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+// (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+System.Console.WriteLine("Введите число: ");
+String InputedString = System.Console.ReadLine();
+int number = Convert.ToInt32(InputedString);
+if (number % 2 == 0)
+{
+    Console.WriteLine($"Число {number} четное");
+}
+else
+{
+    Console.WriteLine($"Число {number} нечетное");
+}
---
+++ TASK4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа
+//  от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+System.Console.WriteLine("Введите число: ");
+String InputedString = System.Console.ReadLine();
+int number = Convert.ToInt32(InputedString);
+int count = 2;
+while (count <= number)
+{
+    Console.Write($"{count} ");
+    count+=2;
+}
